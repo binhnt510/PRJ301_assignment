0e71492163e308e09ffa97e57063b41d0838925a
binhnt510 <binh.nt5124@gmail.com>
2024-10-31 23:16:17 +0700

    add function to block insert twice

--- PRJ_Assignment/src/java/controller/employee/SalaryWorkerController.java
+++ PRJ_Assignment/src/java/controller/employee/SalaryWorkerController.java
@@ -3,8 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 package controller.employee;
+
 import controller.accesscontrol.BaseRBACController;
-import dal.AttendanceReportDBContext;
+import dal.AttendanceReportForSalaryDBContext;
 import dal.SalaryWorkerDBContext;
 import entity.AttendanceReport;
 import entity.Employee;
@@ -14,10 +15,11 @@ import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 
 public class SalaryWorkerController extends BaseRBACController {
-    
+
     private double calculateSalary(AttendanceReport report, int daysInMonth) {
         int totalDays = 0;
         for (int i = 1; i <= daysInMonth; i++) {
@@ -33,8 +35,8 @@ public class SalaryWorkerController extends BaseRBACController {
     protected void doAuthorizedPost(HttpServletRequest req, HttpServletResponse resp, User account) throws ServletException, IOException {
         String date = req.getParameter("monthyear");
         String action = req.getParameter("action");
-        
-        AttendanceReportDBContext db = new AttendanceReportDBContext();
+
+        AttendanceReportForSalaryDBContext db = new AttendanceReportForSalaryDBContext();
         ArrayList<AttendanceReport> reports = db.list1(date);
         int daysInMonth = AttendanceReport.getDaysInMonth(date);
         SalaryWorkerDBContext salaryDB = new SalaryWorkerDBContext();
@@ -46,42 +48,49 @@ public class SalaryWorkerController extends BaseRBACController {
             }
         } else if ("save".equals(action)) {
             // Save salaries to database
-            
-            
-            for (AttendanceReport report : reports) {
-                double calculatedSalary = calculateSalary(report, daysInMonth);
-                
-                SalaryDetail salary = new SalaryDetail();
-                Employee emp = new Employee();
-                emp.setId(report.getEmployeeId());
-                
-                salary.setEmp(emp);
-                salary.setMonthyear(date);
-                salary.setSalary(calculatedSalary);
-                salary.setFine(0.0);
-                salary.setNote(null);
-                
-                salaryDB.insert(salary);
-            }
-            
-            resp.sendRedirect(req.getRequestURI() + "?monthyear=" + date);
+
+            HttpSession session = req.getSession();
+            Boolean insertPageVisited = (Boolean) session.getAttribute("insertPageVisited");
+            if (insertPageVisited != null && insertPageVisited) {
+                for (AttendanceReport report : reports) {
+                    double calculatedSalary = calculateSalary(report, daysInMonth);
+
+                    SalaryDetail salary = new SalaryDetail();
+                    Employee emp = new Employee();
+                    emp.setId(report.getEmployeeId());
+
+                    salary.setEmp(emp);
+                    salary.setMonthyear(date);
+                    salary.setSalary(calculatedSalary);
+                    salary.setFine(0.0);
+                    salary.setNote(null);
+
+                    salaryDB.insert(salary);
+                }
+                session.setAttribute("insertPageVisited", false);
+                req.getRequestDispatcher("/view/employee/salaryinsert.jsp").forward(req, resp);
+                return;
+            }else{
+                req.getRequestDispatcher("/view/employee/salaryinsert.jsp").forward(req, resp);
             return;
+            }
+
         }
         if (salaryDB.existsBySalaryWorkerDate(date)) {
-            req.setAttribute("message", "Salaryworker has added for "+date);
+            req.setAttribute("message", "Salaryworker has added for " + date);
         }
         req.setAttribute("selectedDate", date);
         req.setAttribute("daysInMonth", daysInMonth);
         req.setAttribute("reports", reports);
-        
+        req.setAttribute("action", action);
         req.getRequestDispatcher("/view/employee/salaryinsert.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doAuthorizedGet(HttpServletRequest req, HttpServletResponse resp, User account) throws ServletException, IOException {
+    protected void doAuthorizedGet(HttpServletRequest req, HttpServletResponse resp,
+            User account) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.setAttribute("insertPageVisited", true);
         req.getRequestDispatcher("/view/employee/salaryinsert.jsp").forward(req, resp);
     }
 }
-
-
-
--- PRJ_Assignment/src/java/controller/employee/SchedualEmployeeController.java
+++ PRJ_Assignment/src/java/controller/employee/SchedualEmployeeController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.employee;
 
 import dal.SchedualCampaignDBContext;
@@ -17,30 +16,30 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class SchedualEmployeeController extends HttpServlet {
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
         int scId = Integer.parseInt(request.getParameter("scId"));
         int deptId = Integer.parseInt(request.getParameter("deptId"));
         String shift = request.getParameter("shift");
-        
-        
+
         SchedualCmapainDetailDBContext scDB = new SchedualCmapainDetailDBContext();
         ArrayList<SchedualCampaignDetail> scheduals = scDB.getByDepartment(deptId);
         SchedualCampaignDetail selectedSchedule = null;
-        for(SchedualCampaignDetail sc : scheduals) {
-            if(sc.getScID()== scId) {
+        for (SchedualCampaignDetail sc : scheduals) {
+            if (sc.getScID() == scId) {
                 selectedSchedule = sc;
                 break;
             }
         }
-        
+
         SchedualEmployeeDBContext seDB = new SchedualEmployeeDBContext();
         ArrayList<Employee> employees = seDB.getAvailableEmployees(deptId, shift);
         if (seDB.existsBySchedualEmployeeId(scId)) {
@@ -48,9 +47,10 @@ public class SchedualEmployeeController extends HttpServlet {
         }
         request.setAttribute("schedule", selectedSchedule);
         request.setAttribute("employees", employees);
+        session.setAttribute("insertPageVisited", true);
         request.getRequestDispatcher("../view/work/createschedual.jsp").forward(request, response);
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -58,23 +58,28 @@ public class SchedualEmployeeController extends HttpServlet {
         String[] employeeIds = request.getParameterValues("employeeIds");
         String[] quantities = request.getParameterValues("quantities");
         SchedualEmployeeDBContext seDB = new SchedualEmployeeDBContext();
-        
-        for(int i = 0; i < employeeIds.length; i++) {
-            if(quantities[i] != null && !quantities[i].isEmpty()) {
-                SchedualEmployee se = new SchedualEmployee();
-                SchedualCampaignDetail sc = new SchedualCampaignDetail();
-                sc.setScID(scId);
-                se.setSchedualCampaignDetail(sc);
-                
-                Employee emp = new Employee();
-                emp.setId(Integer.parseInt(employeeIds[i]));
-                se.setEmployee(emp);
-                
-                se.setOrderedQuantity(Integer.parseInt(quantities[i]));
-                seDB.insert(se);
+        HttpSession session = request.getSession();
+        Boolean insertPageVisited = (Boolean) session.getAttribute("insertPageVisited");
+        if (insertPageVisited != null && insertPageVisited) {
+            for (int i = 0; i < employeeIds.length; i++) {
+                if (quantities[i] != null && !quantities[i].isEmpty()) {
+                    SchedualEmployee se = new SchedualEmployee();
+                    SchedualCampaignDetail sc = new SchedualCampaignDetail();
+                    sc.setScID(scId);
+                    se.setSchedualCampaignDetail(sc);
+
+                    Employee emp = new Employee();
+                    emp.setId(Integer.parseInt(employeeIds[i]));
+                    se.setEmployee(emp);
+
+                    se.setOrderedQuantity(Integer.parseInt(quantities[i]));
+                    seDB.insert(se);
+                }
             }
+            session.setAttribute("insertPageVisited", false);
+            response.sendRedirect("listschedulecampaign");
+        } else {
+            response.sendRedirect("listschedulecampaign");
         }
-        
-        response.sendRedirect("listschedulecampaign");
     }
 }
